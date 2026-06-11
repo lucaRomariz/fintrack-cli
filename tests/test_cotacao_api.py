6ff93f659c96ad14496f365bda79d9bf4baf5a5c
lucaRomariz <luca.romariz@icloud.com>
2026-06-10 22:54:56 -0300

    tests: remove dependência da AwesomeAPI nos testes

--- tests/test_cotacao_api.py
+++ tests/test_cotacao_api.py
@@ -1,6 +1,21 @@
+from unittest.mock import patch, Mock
 from fintrack.cotacao_service import obter_cotacao_dolar
 
-def test_obter_cotacao_dolar():
+
+@patch("fintrack.cotacao_service.requests.get")
+def test_obter_cotacao_dolar(mock_get):
+    mock_response = Mock()
+
+    mock_response.json.return_value = {
+        "USDBRL": {
+            "bid": "5.50"
+        }
+    }
+
+    mock_response.raise_for_status.return_value = None
+
+    mock_get.return_value = mock_response
+
     cotacao = obter_cotacao_dolar()
-    assert cotacao is not None
-    assert cotacao > 0
+
+    assert cotacao == 5.50
